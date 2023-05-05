e6db50af5c35481f7a5e54b71527b6ddccbcfffd
Ramzan Demishtigov <rdemistigov@gmail.com>
2023-05-05 18:03:04 +0300

    Final version

--- middleware/notifications.js
+++ middleware/notifications.js
@@ -3,15 +3,12 @@ const util = require('util');
 const log_file = fs.createWriteStream('./debug.log', {flags : 'w'});
 
 function notifCheck(user,doctors){
-    const currentDate = new Date("2022-01-01T08:30:00Z");
+    const currentDate = new Date();
     for(let i=0;i<doctors.length;i++){
     	for(let j=0;j<doctors[i].slots.length;j++){
             // пример времени в докторских слотах "2022-01-01T01:00:00"
             const userDate = new Date(doctors[i].slots[j].time)
             const timeDiff = Math.abs(currentDate - userDate) / 36e5;
-            console.log(timeDiff)
-            console.log(currentDate)
-            console.log(userDate)
             if (timeDiff === 2) {
                 log_file.write(util.format(`${currentDate} | Привет ${user.name} ! Вам через 2 часа к ${ doctors[i].spec } в ${ doctors[i].slots[j].time }!` + '\n'));
             }else if(timeDiff === 24) {
--- routes.js
+++ routes.js
@@ -3,14 +3,48 @@ const router = express.Router()
 const Doctors = require("./models/Doctors.js")
 const Users = require("./models/Users.js")
 const notifCheck = require("./middleware/notifications.js")
-
+//Сервис уведомлений
 router.get("/notifications/:userId",async (req,res) => {
 	const user = await Users.findOne({_id:req.params.userId})
 	let doctors = await Doctors.find()
 	notifCheck(user,doctors)
 	res.send()
 })
-//router.use("/users/:id",notifCheck())
+//Сервис записи к врачу
+router.patch("/doctors/:id", async (req, res) => {
+	const doctor = await Doctors.findOne({ _id: req.params.id })
+	const time = req.body.time
+	let response;
+	for(let i=0;i<doctor.slots.length;i++){
+		if(doctor.slots[i].time == time && doctor.slots[i].condition == false){
+			doctor.slots[i].condition = true;
+			doctor.slots[i].user = req.body.user;
+			await doctor.save()
+			response = doctor
+		}else if(doctor.slots[i].time == time && doctor.slots[i].condition == true){
+			response = "Данное время уже занято"
+		}
+	}
+	if(response != doctor && response != "Данное время уже занято"){
+		response = "Ошибка: Такого времени не существует"
+	}
+	res.send(response)
+})
+
+router.get("/doctors", async (req, res) => {
+	const doctors = await Doctors.find()
+	res.send(doctors)
+})
+
+router.post("/doctors", async (req, res) => {
+	const doctor = new Doctors({
+		name: req.body.name,
+        spec: req.body.spec,
+        slots: [...req.body.slots]
+	})
+	await doctor.save()
+	res.send(doctor)
+})
 
 router.get("/users/:id", async (req, res) => {
 	const user = await Users.findOne({ _id: req.params.id })
@@ -29,41 +63,6 @@ router.post("/users", async (req, res) => {
 	})
 	await user.save()
 	res.send(user)
-})
-
-router.get("/doctors", async (req, res) => {
-	const doctors = await Doctors.find()
-	res.send(doctors)
-})
-
-router.post("/doctors", async (req, res) => {
-	const doctor = new Doctors({
-		name: req.body.name,
-        spec: req.body.spec,
-        slots: [...req.body.slots]
-	})
-	await doctor.save()
-	res.send(doctor)
-})
-
-router.patch("/doctors/:id", async (req, res) => {
-	const doctor = await Doctors.findOne({ _id: req.params.id })
-    const time = req.body.time
-	let response;
-    for(let i=0;i<doctor.slots.length;i++){
-        if(doctor.slots[i].time == time && doctor.slots[i].condition == false){
-            doctor.slots[i].condition = true;
-            doctor.slots[i].user = req.body.user;
-            await doctor.save()
-	        response = doctor
-        }else if(doctor.slots[i].time == time && doctor.slots[i].condition == true){
-            response = "Данное время уже занято"
-        }
-    }
-	if(response != doctor && response != "Данное время уже занято"){
-		response = "Ошибка: Такого времени не существует"
-	}
-	res.send(response)
 })
 
 module.exports = router
